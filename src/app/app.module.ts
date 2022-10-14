d47b01fe5f965ab245875d664fbb8ea27721e788
Ivan <ivanparraruiz1@gmail.com>
2022-10-14 17:21:06 +0200

    initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FiltersComponent } from './filters/filters.component';
-import { IconLinkComponent } from './icon-link/icon-link.component';
-import { ListLinkComponent } from './list-link/list-link.component';
-import { UserComponent } from './user/user.component';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
-import { AsideComponent } from './aside/aside.component';
-import { IconsComponent } from './icons/icons.component';
-import { SearchEngineSelectorComponent } from './search-engine-selector/search-engine-selector.component';
-import { SearchComponent } from './search/search.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FiltersComponent,
-    IconLinkComponent,
-    ListLinkComponent,
-    UserComponent,
-    AsideComponent,
-    IconsComponent,
-    SearchEngineSelectorComponent,
-    SearchComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
